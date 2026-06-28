aaa9e0a577cf88679d99ce4c3d6ae4207a81e704
王云子 <85829302+wangyunzi@users.noreply.github.com>
2026-06-29 01:47:39 +0800

    Update sidebar.ts

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -9,25 +9,75 @@ export default sidebar({
     // 读书笔记架构更换到 docsify，不能使用相对链接
     // { text: "读书笔记", icon: "fa6-brands:readme", link: "https://newzone.top/reading/" },
     // 指定显示页面
-    {
-    text: "📖 读书笔记",
-    icon: "",
-    prefix: "reading/",
-    collapsible: true,
-    children: [
-      {
-          text: "其他",
-          icon: "fa6-solid:code-compare",
-          collapsible: true,
-          children: ["design.md"],
-        },
-    ],
-    },
+    { text: "读书笔记", icon: "fa6-brands:readme", link: "/reading/" },
+    { text: "应用手册", icon: "", link: "/apps/" },
+    // { text: "页面开发", icon: "", link: "/web/" },
+    { text: "网站部署", icon: "", link: "/deploy/" },
+    { text: "代码编程", icon: "", link: "/code/" },
+    // { text: "生活记录", icon: "", link: "/family/" },
+    { text: "加密目录", icon: "material-symbols:encrypted", link: "/encrypt/" },
+    // { text: "博客文章", icon: "fa6-solid:feather-pointed", link: "/blog" },
+  ],
 
+  "/reading/": [
+    {
+      text: "📖 读书笔记",
+      icon: "",
+      prefix: "",
+      collapsible: true,
+      children: [
+        "README.md",
+        {
+          text: "效率与习惯",
+          icon: "fa6-solid:bolt",
+          prefix: "0_效率与习惯/",
+          collapsible: true,
+          children: "structure",
+        },
+        {
+          text: "思维与成长",
+          icon: "fa6-solid:seedling",
+          prefix: "1_思维与成长/",
+          collapsible: true,
+          children: "structure",
+        },
+        {
+          text: "沟通与影响",
+          icon: "fa6-solid:comments",
+          prefix: "2_沟通与影响/",
+          collapsible: true,
+          children: "structure",
+        },
+        {
+          text: "创意与写作",
+          icon: "fa6-solid:pen-nib",
+          prefix: "3_创意与写作/",
+          collapsible: true,
+          children: "structure",
+        },
+        {
+          text: "健康与生活",
+          icon: "fa6-solid:heart-pulse",
+          prefix: "4_健康与生活/",
+          collapsible: true,
+          children: "structure",
+        },
+        {
+          text: "商业与社会",
+          icon: "fa6-solid:briefcase",
+          prefix: "5_商业与社会/",
+          collapsible: true,
+          children: "structure",
+        },
+      ],
+    },
+  ],
+
+  "/apps/": [
     {
       text: "🧰 应用手册",
       icon: "",
-      prefix: "apps/",
+      prefix: "",
       collapsible: true,
       children: [
         "Applist.md",
@@ -41,17 +91,23 @@ export default sidebar({
         },
       ],
     },
-    // {
-    //   text: "🌐 页面开发",
-    //   icon: "",
-    //   prefix: "web/",
-    //   collapsible: true,
-    //   children: "structure",
-    // },
+  ],
+
+  // "/web/": [
+  //   {
+  //     text: "🌐 页面开发",
+  //     icon: "",
+  //     prefix: "",
+  //     collapsible: true,
+  //     children: "structure",
+  //   },
+  // ],
+
+  "/deploy/": [
     {
       text: "🏗️ 网站部署",
       icon: "",
-      prefix: "deploy/",
+      prefix: "",
       collapsible: true,
       children: [
         "Static.md",
@@ -65,10 +121,13 @@ export default sidebar({
         },
       ],
     },
+  ],
+
+  "/code/": [
     {
       text: "🔡 代码编程",
       icon: "",
-      prefix: "code/",
+      prefix: "",
       collapsible: true,
       children: [
         "README.md",
@@ -86,43 +145,33 @@ export default sidebar({
         },
       ],
     },
-    // {
-    //   text: "🛖 生活记录",
-    //   icon: "",
-    //   prefix: "family/",
-    //   collapsible: true,
-    //   children: "structure",
-    // },
+  ],
+
+  // "/family/": [
+  //   {
+  //     text: "🛖 生活记录",
+  //     icon: "",
+  //     prefix: "",
+  //     collapsible: true,
+  //     children: "structure",
+  //   },
+  // ],
+
+  "/encrypt/": [
     {
       text: "加密目录",
       icon: "material-symbols:encrypted",
-      prefix: "encrypt/",
+      prefix: "",
       collapsible: true,
       children: "structure",
     },
-    // {
-    //   text: "博客文章",
-    //   icon: "fa6-solid:feather-pointed",
-    //   prefix: "_posts/",
-    //   link: "/blog",
-    //   collapsible: true,
-    //   children: "structure",
-    // },
   ],
-
-
 
   // 专题区（独立侧边栏）
   "/apps/topic/": "structure",
-  "/reading/0_效率与习惯/": "structure",
-  "/reading/1_思维与成长/": "structure",
-  "/reading/2_沟通与影响/": "structure",
-  "/reading/3_创意与写作/": "structure",
-  "/reading/4_健康与生活/": "structure",
-  "/reading/5_商业与社会/": "structure",
 
   // 如果你不想使用默认侧边栏，可以按照路径自行设置。但需要去掉下方配置中的注释，以避免博客和时间轴出现异常。_posts 目录可以不存在。
-  /*"/_posts/": [
+  "/_posts/": [
     {
       text: "博客文章",
       icon: "fa6-solid:feather-pointed",
@@ -131,5 +180,5 @@ export default sidebar({
       collapsible: true,
       children: "structure",
     },
-  ], */
+  ],
 });
