50299f49037d9991c2e54e236c533fb317aefdae
王云子 <85829302+wangyunzi@users.noreply.github.com>
2026-06-29 01:41:24 +0800

    Update sidebar.ts

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -11,11 +11,17 @@ export default sidebar({
     // 指定显示页面
     {
     text: "📖 读书笔记",
-    icon: "fa6-brands:readme",
+    icon: "",
     prefix: "reading/",
-    link: "/reading/",
     collapsible: true,
-    children: "structure",
+    children: [
+      {
+          text: "其他",
+          icon: "fa6-solid:code-compare",
+          collapsible: true,
+          children: ["design.md"],
+        },
+    ],
     },
 
     {
@@ -108,6 +114,12 @@ export default sidebar({
 
   // 专题区（独立侧边栏）
   "/apps/topic/": "structure",
+  "/reading/0_效率与习惯/": "structure",
+  "/reading/1_思维与成长/": "structure",
+  "/reading/2_沟通与影响/": "structure",
+  "/reading/3_创意与写作/": "structure",
+  "/reading/4_健康与生活/": "structure",
+  "/reading/5_商业与社会/": "structure",
 
   // 如果你不想使用默认侧边栏，可以按照路径自行设置。但需要去掉下方配置中的注释，以避免博客和时间轴出现异常。_posts 目录可以不存在。
   /*"/_posts/": [
